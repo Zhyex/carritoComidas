e334891e80175a9560caffc61cf21c851aa947af
Camilo Villegas <jvillear@emeal.nttdata.com>
2025-03-08 08:21:38 -0500

    resumen datos del pedido

--- js/thankyou.js
+++ js/thankyou.js
@@ -1,4 +1,6 @@
 let ordenProductos = document.querySelector('.list-cartOrder tbody')
+let resumenPedido = document.querySelector('.resumenPedido')
+let datosPedido = document.querySelector('.datosPedido')
 let btnRestart = document.querySelector('.btn-gracias')
 document.addEventListener('DOMContentLoaded',()=>{
     mostrarPedido();
@@ -7,6 +9,12 @@ document.addEventListener('DOMContentLoaded',()=>{
 function mostrarPedido(){
     let resumenCompra = JSON.parse(localStorage.getItem('finalizaCompra')) || [];
     let productos = Object.values(resumenCompra).filter(value => typeof value === "object" && value !== null && !Array.isArray(value));
+    resumenPedido.innerHTML =`
+        <h2>Hola, ${resumenCompra.nombre} ${resumenCompra.apellido}</h2>
+        <p class="mb-32">Estamos preparando tu pedido, pronto nos comunicaremos contigo para realizar la entrega.</p>
+        <p>Aquí tienes un resumen de tu compra</p>
+    `
+    ordenProductos.innerHTML = '';
     productos.forEach((producto) => {
         let row = document.createElement('tr');
         row.innerHTML = `
@@ -25,8 +33,20 @@ function mostrarPedido(){
         `;
         ordenProductos.appendChild(row);
     });
+    datosPedido.innerHTML = `
+        <p>Cliente: ${resumenCompra.nombre} ${resumenCompra.apellido}</p>
+        <p>Correo: ${resumenCompra.correo}</p>
+        <p>Telefono: ${resumenCompra.telefono}</p>
+        <p>Dirección: ${resumenCompra.direccion}</p>
+        <p>Ciudad domiclio: ${resumenCompra.destino}</p>
+        <p>Datos adicionales: ${resumenCompra.datosAdicionales}</p>
+        <p>Valor del domicilio: ${resumenCompra.domicilio}</p>
+        <p>Valor del descuento: ${resumenCompra.descuento}</p>
+        <p>Subtotal: ${resumenCompra.subtotal}</p>
+        <p>Total a pagar(Método de pago inlcuido): ${resumenCompra.total}</p>
+    `
 }
 btnRestart.addEventListener('click',()=>{
-    localStorage.clear();
-    location.href='index.html'
+    //localStorage.clear();
+    //location.href='index.html'
 });
